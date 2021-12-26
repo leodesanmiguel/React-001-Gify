a35614e673276c06c5220a7dcc481487721be3dc
Leo <leodesanmiguel@gmail.com>
2021-12-26 20:23:01 -0300

    Agregando el titulo y un Id

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import getGifs from './services/getGifs';
-
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import getGifs from "./services/getGifs";
 
 function App() {
-    const [gifs, setGifs] = useState([]);
+  const [gifs, setGifs] = useState([]);
 
-    useEffect(function() {
+  useEffect(function() {
+    getGifs({ keyword: "matrix" }).then(gifs => setGifs(gifs));
+  }, []);
 
-        getGifs({ keyword: 'matrix' }).then(gifs => setGifs(gifs));
+  return (
+    <div className="App">
+      <section className="App-content">
+        {gifs.map(singleGif => {
+          return (
+            <div>
+              <h4>{singleGif.title}</h4>
+              <img alt={singleGif.title} src={singleGif.url} /> <br />
+              <small>{singleGif.id}</small>
+            </div>
+          );
+        })}
+      </section>
+    </div>
+  );
+}
 
-    }, []);
-
-
-    return ( <
-        div className = "App" >
-        <
-        section className = "App-content" > {
-            gifs.map(singleGif =>
-                <
-                img src = { singleGif }
-                />)
-            } <
-            /section >  < /
-            div >
-        );
-    }
-
-    export default App;
+export default App;
--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,17 +1,19 @@
-const apiKey = 'XBXAw6y6bjgTEFBWeS5oAj2dNvquIrTv'
+const apiKey = "XBXAw6y6bjgTEFBWeS5oAj2dNvquIrTv";
 
+export default function getGifs({ keyword = "morty" } = {}) {
+  const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=10&offset=0&rating=g&lang=en`;
 
-export default function getGifs({ keyword = 'morty' } = {}) {
-    const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=10&offset=0&rating=g&lang=en`;
-
-    return fetch(apiURL)
-        .then(res => res.json())
-        .then(response => {
-            const { data = [] } = response;
-            if (Array.isArray(data)) {
-                const gifs = data.map(imagenes => imagenes.images.downsized_medium.url);
-                //setGifs(gifs);
-                return gifs;
-            }
-        })
+  return fetch(apiURL).then(res => res.json()).then(response => {
+    const { data = [] } = response;
+    if (Array.isArray(data)) {
+      const gifs = data.map(imagenes => {
+        const { images, title, id } = imagenes;
+        const { url } = images.downsized_medium;
+        return { title, id, url };
+        //imagenes.images.downsized_medium.url);
+      });
+      //setGifs(gifs);
+      return gifs;
+    }
+  });
 }
